5fc067b71d7d0b1c3c9a7be677a1b233fe45fc67
Robert Brautigam <robert.brautigam.gyula@gmail.com>
2017-02-22 21:01:04 +0100

    Add display of board for console

--- src/main/java/com/github/robertbraeutigam/tictactoe/console/ConsoleUI.java
+++ src/main/java/com/github/robertbraeutigam/tictactoe/console/ConsoleUI.java
@@ -3,6 +3,7 @@ package com.github.robertbraeutigam.tictactoe.console;
 import com.github.robertbraeutigam.tictactoe.human.UI;
 import com.github.robertbraeutigam.tictactoe.Position;
 import static java.util.Arrays.asList;
+import java.util.stream.Collectors;
 
 public final class ConsoleUI implements UI {
    private final Character mineMark;
@@ -15,12 +16,31 @@ public final class ConsoleUI implements UI {
 
    @Override
    public void drawBoard(Mark[][] board) {
-      System.console().printf("TODO Board\n");
+      asList(board).forEach(this::drawRow);
+   }
+
+   private void drawRow(Mark[] row) {
+      System.console().printf(
+            asList(row).stream()
+            .map(this::markToDisplayMark)
+            .collect(Collectors.joining("  "))
+         +"\n");
+   }
+
+   private String markToDisplayMark(Mark mark) {
+      switch (mark) {
+         case MINE:
+            return ""+mineMark;
+         case ENEMYS:
+            return ""+enemysMark;
+         default:
+            return ".";
+      }
    }
 
    @Override
    public Position askForMove() {
-      String positionInput = System.console().readLine("Enter your move (x,y): ");
+      String positionInput = System.console().readLine("Enter your move '"+mineMark+"' (x,y): ");
       return new Position(Integer.parseInt(positionInput.split(",")[0]), Integer.parseInt(positionInput.split(",")[1]));
    }
 }
